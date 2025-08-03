1edbb951aa45d5273500affd0fb9ab3f0455805d
qsire2 <158807774+qsire2@users.noreply.github.com>
2025-08-03 11:08:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -147,3 +148,4 @@ def Civilization():
     #app.run(debug=True)
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port, debug=False)
+
